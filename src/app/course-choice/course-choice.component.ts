0acb8dfcc9455e56019ac010ea4ff28e2705868a
Willian Pacheco <willian.pacheco@solidaridad.org>
2021-03-01 22:11:06 -0300

    exerc1 semana2 - components

---
+++ src/app/course-choice/course-choice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-choice',
+  templateUrl: './course-choice.component.html',
+  styleUrls: ['./course-choice.component.css']
+})
+export class CourseChoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
